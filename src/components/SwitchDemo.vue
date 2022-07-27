d3de5cc084b6085efd930530294ff6d575b8e2ad
king-open <kingwang.0624@gmail.com>
2022-07-27 16:42:43 +0800

    路由关闭之后切换 aside

---
+++ src/components/SwitchDemo.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    SwitchDemo.vue
+  </div>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.scss'
-import Home from './views/Home.vue'
-import Doc from './views/Doc.vue'
-import { createWebHashHistory,createRouter } from 'vue-router'
-
-const histroy = createWebHashHistory()
-const router = createRouter({
-  history:histroy,
-  routes:[
-    {path:'/',component:Home},
-    {path:'/doc',component:Doc}
-  ]
-})
+import {router} from './router'
 
 const app = createApp(App)
 app.use(router)
