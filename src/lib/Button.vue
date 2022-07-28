001112ca0b37a96a33e8a51f20a9604f900235cf
king-open <kingwang.0624@gmail.com>
2022-07-28 13:17:15 +0800

    CSS最小影响原则

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="sky-button" :class="{ [`theme-${theme}`]: theme }">
+  <button class="sky-button" :class="{ [`sky-theme-${theme}`]: theme }">
     <slot />
   </button>
 </template>
@@ -16,7 +16,7 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="toggle" :class="{checked:value}">
+  <button class="sky-switch" @click="toggle" :class="{'sky-checked':value}">
     <span></span>
   </button>
 </template>
@@ -20,21 +20,21 @@ export default{
 
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 22px;
   $h2: $h - 4px;
-  button{height: $h;width: $h*2;border: none;background: #bfbfbf;border-radius: $h/2;position: relative;
+  .sky-switch{height: $h;width: $h*2;border: none;background: #bfbfbf;border-radius: $h/2;position: relative;
   > span{
     position: absolute;top: 2px;left: 2px; height: $h2;width: $h2;background:white;border-radius: $h2 / 2; transition: all 250ms;
     }
-    &.checked{background: #1890ff;
+    &.sky-checked{background: #1890ff;
     > span {left: calc(100% - #{$h2} - 2px);}
     }
      &:focus{ outline: none;}
      &:active{
     > span {width: $h2 + 4px;}
     }
-    &.checked:active{
+    &.sky-checked:active{
     > span {width: $h2 + 4px; margin-left: -4px;}
   }
   }
