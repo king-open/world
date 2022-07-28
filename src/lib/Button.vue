983f528a44fca5ef48384920ecf859ef74e1194d
king-open <kingwang.0624@gmail.com>
2022-07-28 12:58:17 +0800

    创建button

---
+++ src/lib/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :size="size">
+    <button v-bind="reset" >
+      <slot />
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+export default{
+  inheritAttrs:false,
+  props:{},
+  setup(props,context){
+    const {size,...reset} = context.attrs;
+    return {size,reset}
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+div{
+  border: 1px solid;
+}
+</style>
