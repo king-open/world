a296d1bff7a083b619bce4a0a9e5ed20a90ccdb6
king-open <kingwang.0624@gmail.com>
2022-07-28 13:18:28 +0800

    支持 theme 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -50,5 +50,21 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+  &.sky-theme-link{
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,&:focus{
+      color: lighten($blue, 10%);
+    }
+  }
+  &.sky-theme-text{
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,&:focus{
+      background: darken(white, 5%);;
+    }
+  }
 }
 </style>
